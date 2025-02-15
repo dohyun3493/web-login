83a84fe23a336e5e1365b517c8f79ca213f1ccca
dodo <wjddmsehgus@naver.com>
2025-02-15 15:34:36 +0900

    DOM을 통해 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use stirct"
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginbutton = document.querySelector("button");
+
+loginbutton.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    }
+    console.log(req)
+}
